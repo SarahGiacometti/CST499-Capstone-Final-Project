5a9a9f613263d71cf34729da7df4232ae16a29d9
SarahGiacometti <88863532+SarahGiacometti@users.noreply.github.com>
2021-09-09 19:22:26 -0500

    Update courseEnroller.php

--- courseEnroller.php
+++ courseEnroller.php
@@ -9,7 +9,6 @@ error_reporting(E_ALL ^ E_NOTICE)
 if(isset($_POST['save']))
 {
 	$sql="INSERT INTO tblregistration (uname, cname) VALUES('$_POST[email]','$_POST[cname]')";
-
 	
 	if(mysqli_query($con, $sql))
 	{
@@ -19,29 +18,24 @@ if(isset($_POST['save']))
 	{
 		echo "Error: " .$sql."
 		" .mysql_error($con);
-	}
-	
+	}	
 	echo '<table border = "1" cellspacing="2" cellpadding = "2">
 			<tr>
 				<td><font face = "Arial">Username:  </font> </td>
 				<td><font face = "Arial">Course Name:  </font> </td>
 				</tr>';
-	
 	if($result = $con->query($query))
 	{
 		while($row = $result->fetch_assoc())
 		{
 			$id = $row["email"];
 			$firstName = $row["cname"];
-
-		
 			
 			echo '<tr>
 				<td>'.$email.'</td>
 				<td>'.$cname.'</td>
 				</tr>';
 		}
-		
 		$result -> free();
 	}
 }
